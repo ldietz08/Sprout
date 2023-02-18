c8959d64075ab7d7ed7b4fafd32e175da5f731b1
Lakpa Dietz <ldietz086@gmail.com>
2023-02-17 21:39:33 -0800

    Added create post controller and favicon

---
+++ controllers/create.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getCreate: (req, res) => {
+    res.render("create.ejs");
+  },
+};
--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const createController = require("../controllers/create");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
+router.get("/create", ensureAuth, createController.getCreate);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
